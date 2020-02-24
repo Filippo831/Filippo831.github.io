5a4d13f9ab7f5cae3022c92757a55833479f630e
Filippo831 <filippoarduini832@gmail.com>
2020-02-24 21:15:53 +0100

    avanti

--- development/js/navbar.js
+++ development/js/navbar.js
@@ -6,55 +6,51 @@ let burger = document.querySelector(".burger");
 let timingAnimation;
 let lenghtWindow = window.innerWidth;
 let navbarWhiteBgLinks = document.querySelector(".navbarMenuSocial");
-let navbarWhiteBgLogo = window.getComputedStyle(
-    document.querySelector('.underNav'), ':before'
-);
-
-
+let navbarWhiteBgLogo = document.querySelector(".underNav");
 
 //  on click set state grid
 burger.addEventListener("click", () => {
     gsap.to(".navbarMenuSocial", {
-        duration: .3,
+        duration: 0.3,
         display: navbarMenu.display == "grid" ? "none" : "grid",
         x: navbarMenu.display == "grid" ? lenghtWindow : 0,
+        opacity: navbarMenu.display == "grid" ? 1 : 0.8
     });
     burgerToggle();
 });
-
 
 function burgerToggle() {
     if (navbarMenu.display != "grid") {
         gsap.to(".line1", {
             rotate: 45,
-            y: 7,
-        })
+            y: 7
+        });
         gsap.to(".line3", {
             rotate: -45,
-            y: -7,
-        })
+            y: -7
+        });
         gsap.to(".line2", {
             duration: 0,
-            opacity: 0,
-        })
+            opacity: 0
+        });
         gsap.to(".navbarMenuSocial", {
-            opacity: 1,
-        })
+            opacity: 1
+        });
     } else {
         gsap.to(".line1", {
             rotate: 0,
-            y: 0,
-        })
+            y: 0
+        });
         gsap.to(".line3", {
             rotate: 0,
-            y: 0,
-        })
+            y: 0
+        });
         gsap.to(".line2", {
-            duration: .3,
-            opacity: 1,
-        })
+            duration: 0.3,
+            opacity: 1
+        });
         gsap.to(".navbarMenuSocial", {
-            opacity: 1,
-        })
+            opacity: 1
+        });
     }
 }
